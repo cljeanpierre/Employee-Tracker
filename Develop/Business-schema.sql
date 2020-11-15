fb349ed9be97d47490569ae05df7c22f64331558
Carla <carla@layc-dc.org>
2020-11-15 01:50:25 -0500

    added auto increments for primary key IDs

--- Develop/Business-schema.sql
+++ Develop/Business-schema.sql
@@ -4,26 +4,23 @@ CREATE database company_profilesDB;
 USE company_profilesDB;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   department_name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
 );
 
 CREATE TABLE department_role (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) NULL,
   salary DECIMAL(10,4) NULL,
   department_id INT NULL,
-  PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INT NULL,
   manager_id INT NULL,
-  PRIMARY KEY (id)
 );
 
 SELECT * FROM department;
