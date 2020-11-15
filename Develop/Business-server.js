100502f9fefd722568505a22681c90231632939f
Carla <carla@layc-dc.org>
2020-11-15 01:51:16 -0500

    replaced if/else statements with switch statements

--- Develop/Business-server.js
+++ Develop/Business-server.js
@@ -15,3 +15,74 @@ var connection = mysql.createConnection({
   password: "Iluvbugs2!!",
   database: "company_profilesDB"
 });
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+// run the start function after the connection is made to prompt the user
+  start();
+});
+
+// function which prompts the user for what action they should take
+function start() {
+  inquirer
+    .prompt({
+      name: "typeOfQuery",
+      type: "list",
+      message: "What kind of query would you like to make?",
+      choices: ["add department", "add roles",  "add employees", "view department", "view roles", "view employees", "update employee roles", "quit"]
+    })
+    .then(function(answer) {
+      // based on user answer, post these functions
+    switch (answer.choices) {
+      case 'add department':
+        return addDepartment();
+        break;
+
+        case 'add roles':
+        return addRoles();
+        break;
+
+        case 'add employees':
+        return addEmployees();
+        break;
+
+        case 'view department':
+        return viewDepartment();
+        break;
+
+        case 'view roles':
+        return viewRoles();
+        break;
+
+        case 'view employees':
+        return viewEmployees();
+        break;
+
+        case 'updateemployeeroles':
+        return updateEmployeeRoles();
+        break;
+
+        default: 'quit'
+        console.log("You may close the application.");
+      
+    }
+
+    });
+}
+
+function addDepartment() {
+  inquirer.prompt({
+    name: "department",
+    type: "input",
+    message: "What is the name of the department?"
+  }).then(function (answers) {
+    const deptAns = answers.department;
+    const query = "SELECT * FROM" 
+    connection.query(query, deptAns, function(err){
+      if(err) throw err;
+      
+    })
+  } 
+  )
+}
