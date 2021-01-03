38a8c0058d3b0c3de48de804922d216f0fc40c76
Carla <carla@layc-dc.org>
2021-01-03 06:17:43 -0500

    modified code blocks to eliminate repetitive insertions

--- Develop/Seed.sql
+++ Develop/Seed.sql
@@ -1,35 +1,16 @@
 INSERT INTO department (department_name)
-VALUES ("Finance");
-
-INSERT INTO department (department_name)
-VALUES ("Technology");
-
-INSERT INTO department (department_name)
-VALUES ("Human Resources");
-
-INSERT INTO department (department_name)
-VALUES ("Marketing");
+VALUES ("Finance"), ("Technology"), ("Human Resources"), ("Marketing");
 
 INSERT INTO department_role (title, salary, department_id)
-VALUES ("Research Analyst","90,000", "12");
-
-INSERT INTO department_role (title, salary, department_id)
-VALUES ("Treasurer","180,000", "32");
-
-INSERT INTO department_role (title, salary, department_id)
-VALUES ("Personnel Manager","65,000", "5");
-
-INSERT INTO department_role (title, salary, department_id)
-VALUES ("Recruiter","45,000", "8");
+VALUES 
+("Research Analyst","90,000", "12"), 
+("Treasurer","180,000", "32"),  
+("Personnel Manager","65,000", "5"), 
+("Recruiter","45,000", "8");
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Selam","Harding", "7", "16");
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Mandy","Brown", "19", "22");
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John","Whitaker", "37", "14");
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Matthieu","Simmons", "48", "17");
+VALUES 
+("Selam","Harding", "7", "16"),
+("Mandy","Brown", "19", "22"),
+("John","Whitaker", "37", "14"),
+("Matthieu","Simmons", "48", "17");
